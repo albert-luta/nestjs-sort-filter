e9f68ed9ea1763c93a3b9af0f01e96e25a6a2c3a
Albert Luta <luta.albert100@gmail.com>
2022-03-23 00:00:31 +0200

    feat(validation): add automatic validation
    Add the automatica validation and transformation pipe.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -7,6 +7,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   const configService = app.get(ConfigService);
 
